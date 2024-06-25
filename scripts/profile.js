2bd1e0dd2996edcf8e5a22434e589868e06c1e5a
Ian Nduta <gathonjiandutai@gmail.com>
2024-06-25 12:40:35 -0400

    formated the profile.js

---
+++ scripts/profile.js
@@ -0,0 +1,15 @@
+"use strict";
+const userName = document.getElementById("userName");
+const userFullName = document.getElementById("userFullName");  
+const userBio = document.getElementById("userBio");  
+const accountCreated = document.getElementById("accountCreated");  
+const acountUpdated = document.getElementById("acountUpdated");  
+
+
+window.onload = function(){
+    console.log("it works");
+}
+
+function getLogingUserData(){
+
+}
